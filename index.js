34e2991c893cc449afa648a969f1b5bf41c8a1ec
Igor Lopes <igordeagostin@gmail.com>
2018-09-04 13:48:42 -0300

    App

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const PORT = process.env.PORT || 5000
+var server = http.createServer( function(req, res){
+
+    var categoria = req.url;
+
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Notícias de Tecnologia</body></html>");
+    } else if(categoria == '/moda'){
+        res.end("<html><body>Notícias de Moda</body></html>");
+    }else if(categoria == '/beleza'){
+        res.end("<html><body>Notícias de Beleza</body></html>");
+    }else{
+        res.end("<html><body>Portal de noticias</body></html>");
+    }
+});
+
+server.listen(PORT);
